b71a5ecd0b3ec5e96260f21e6531c251e2a13c63
fespschulte <fschultepinto@gmail.com>
2024-06-16 17:59:44 -0300

    :sparkles: Add Handle Order Button Click Function

--- src/main.js
+++ src/main.js
@@ -4,3 +4,20 @@ function resetSelections() {
     selectedBroth = null
     selectedProtein = null
 }
+
+function handleOrderButtonClick() {
+    if (selectedBroth && selectedProtein) {
+        createOrder(selectedBroth, selectedProtein)
+            .then((order) => {
+                console.log('Order created successfully:', order)
+                renderOrderConfirmationPage(order)
+            })
+            .catch((error) => {
+                alert('Failed to create order. Please try again.')
+                console.error('Create Order Error:', error)
+            })
+    } else {
+        alert('Please select both a broth and a protein.')
+    }
+}
+
