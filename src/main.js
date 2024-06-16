5cbbcf76ef2ae6b6911b03c9b944e8da4def26be
fespschulte <fschultepinto@gmail.com>
2024-06-16 17:59:00 -0300

    :sparkles: Add Reset Selections Function

--- src/main.js
+++ src/main.js
@@ -1,9 +1,6 @@
 import './styles/style.css'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <p class="read-the-docs">
-      Hello World!
-    </p>
-  </div>
-`
+function resetSelections() {
+    selectedBroth = null
+    selectedProtein = null
+}
