cbaf66efe799f319cab2f5d54fcef63aa8d82da9
fespschulte <fschultepinto@gmail.com>
2024-06-16 17:49:21 -0300

    :alien: Add List Proteins Function to fetch proteins from API

--- src/api/orderApi.js
+++ src/api/orderApi.js
@@ -1,3 +1,15 @@
+export async function listProteins() {
+    const response = await fetch(`${API_BASE_URL}/proteins`, {
+        method: 'GET',
+        headers: { 'x-api-key': API_KEY },
+    })
+    if (!response.ok) {
+        throw new Error('Error fetching Proteins List')
+    }
+    const proteins = await response.json()
+    return proteins
+}
+
 export async function listBroths() {
     const response = await fetch(`${API_BASE_URL}/broths`, {
         method: 'GET',
