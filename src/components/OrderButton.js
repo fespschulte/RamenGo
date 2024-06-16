48030091a972c5a8a8716255fefe8e29b7d9dc5e
fespschulte <fschultepinto@gmail.com>
2024-06-16 17:57:38 -0300

    :art: Add Order Button Component

---
+++ src/components/OrderButton.js
@@ -0,0 +1,10 @@
+export default function OrderButton() {
+    return `
+    <div class="order-button-container">
+      <button class="button button-order">
+        PLACE MY ORDER
+        <img class="button-arrow" src="./assets/arrow-button-white.svg" alt="Arrow" />
+      </button>
+    </div>
+  `
+}
