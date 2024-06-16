431a6eeea314c493a9c948d7a6e3039e7575dcb7
fespschulte <fschultepinto@gmail.com>
2024-06-16 17:49:52 -0300

    :alien: Add Create Order Function to fetch order from API

--- src/api/orderApi.js
+++ src/api/orderApi.js
@@ -1,3 +1,34 @@
+const API_BASE_URL = '/api'
+const API_KEY = 'ZtVdh8XQ2U8pWI2gmZ7f796Vh8GllXoN7mr0djNf'
+
+export async function createOrder(brothId, proteinId) {
+    try {
+        const requestBody = JSON.stringify({
+            brothId,
+            proteinId,
+        })
+
+        const response = await fetch(`${API_BASE_URL}/orders`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'x-api-key': API_KEY,
+            },
+            body: requestBody,
+        })
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok')
+        }
+
+        const data = await response.json()
+        return data
+    } catch (error) {
+        console.error('Create Order Error:', error)
+        throw error
+    }
+}
+
 export async function listProteins() {
     const response = await fetch(`${API_BASE_URL}/proteins`, {
         method: 'GET',
